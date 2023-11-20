2833c17657de6146645f5a04bc7583f75c0bb834
Marina Konovalchik <marina200014@yandex.by>
2023-11-21 00:17:17 +0300

    updated testbase

--- src/test/java/com/openfoodfacts/web/tests/TestBase.java
+++ src/test/java/com/openfoodfacts/web/tests/TestBase.java
@@ -1,26 +1,32 @@
 package com.openfoodfacts.web.tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.openfoodfacts.web.config.WebDriverProvider;
+import com.openfoodfacts.web.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
-//    @BeforeAll
-//    static void setUp() {
-//        WebDriverProvider.config();
-//    }
+    @BeforeAll
+    static void setUp() {
+        WebDriverProvider.config();
+    }
 
     @BeforeEach
     void addListener() {
-     //   SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         open("https://world.openfoodfacts.org");
     }
 
-//    @AfterEach
-//    void addAttachments(){
-//        Attach.screenshotAs("Last screenshot");
-//        Attach.pageSource();
-//        Attach.browserConsoleLogs();
-//        Attach.addVideo();
-//    }
+    @AfterEach
+    void addAttachments(){
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
 }
